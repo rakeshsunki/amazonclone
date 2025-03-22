fcd2e89bdf9d36347d3c330f8e05b34679106c36
rakesh sunki <rakeshsunkiOfficial@gmail.com>
2025-03-22 17:03:22 +0530

    changed login page to justify- start

--- src/components/loginpage/loginpage.jsx
+++ src/components/loginpage/loginpage.jsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
 const Login = () => {
   return (
-    <div className="h-screen flex flex-col justify-center items-center">
-      <img src="./AmazonBlacklogo.webp" className="h-[7rem] inline"></img>
-      <span>.in</span>
+    <div className="h-screen flex flex-col justify-center items-start">
+      <span>
+        <img src="./AmazonBlacklogo.webp" className="h-[7rem] inline"></img>
+        <span>.in</span>
+      </span>
 
       <div className="p-[1.5rem] bg-white w-[22rem] rounded-[0.5rem] border-1 border-gray-300 text-sm">
         <form>
@@ -30,7 +32,7 @@ const Login = () => {
         </p>
         <Link to={"/"}>Shop on Amazon Business</Link>
       </div>
-      <div className="justify-items-center text-sm m-[1rem]">
+      <div className="flex flex-col justify-center text-sm m-[1rem]">
         <p>New to Amazon ?</p>
         <button className="border-1 border-gray-500 w-[15rem] h-[1.5rem] !rounded-lg">
           Create your Amazon account
