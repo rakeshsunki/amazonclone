5d5cbc5435fe5793d937ea7456116a224a5804b9
rakesh sunki <rakeshsunkiOfficial@gmail.com>
2025-06-13 20:23:04 +0530

    updated loader

--- src/components/Loader/loader.jsx
+++ src/components/Loader/loader.jsx
@@ -2,6 +2,7 @@ const Loader = () => {
   return (
     <>
       <center className="text-center">
+        <h2>The Depolyed Server Is Initially Slow Please Wait </h2>
         <div className="spinner-border" role="status">
           <span className="visually-hidden">Loading...</span>
         </div>
